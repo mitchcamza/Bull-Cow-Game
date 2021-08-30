f509900c9256c86cb237542c3fb10f8e25958be8
mitchcamza <192mitch88@gmail.com>
2021-08-30 17:40:26 +0200

    Variables and cin for Input

--- 02_BullCowGame/BullCowGame/main.cpp
+++ 02_BullCowGame/BullCowGame/main.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
+	// introduce the game
 	constexpr int WORD_LENGTH = 5;
 	cout << "Welcome to Bulls and Cows - a fun word game\n";
 	cout << "Can you guess the " << WORD_LENGTH;
 	cout << " letter isogram I'm thinking of?\n";
+	cout << endl;
+
+	// get a guess from the player
+	cout << "Enter your guess: ";
+	string Guess = "";
+	cin >> Guess;
+
+	// repeat the guess back to them
+	cout << "Your guess was: " << Guess << endl;
+
+	// two word guesses cause an error
+	// cin strips trailing whitespace and the second word is left in the input stream
+	cout << "Enter your guess: ";
+	cin >> Guess;
+	cout << "Your guess was: " << Guess << endl;
 
 	return 0;
 }
