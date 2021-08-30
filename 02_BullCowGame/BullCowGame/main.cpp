d07f1a89be5b4d7d9f76760aa79156b5b6b4c8ea
mitchcamza <192mitch88@gmail.com>
2021-08-30 18:09:26 +0200

    Simplifying with Functions

--- 02_BullCowGame/BullCowGame/main.cpp
+++ 02_BullCowGame/BullCowGame/main.cpp
@@ -2,29 +2,41 @@
 #include <string>
 using namespace std;
 
+// Function Prototypes
+void PrintIntro();
+string GetGuessAndPrintBack();
+
+// Entry point for our application
 int main()
 {
-	// introduce the game
+	PrintIntro();
+	GetGuessAndPrintBack();
+	GetGuessAndPrintBack();
+	cout << endl;
+
+	return 0;
+}
+
+// introduce the game
+void PrintIntro()
+{
 	constexpr int WORD_LENGTH = 5;
 	cout << "Welcome to Bulls and Cows - a fun word game\n";
 	cout << "Can you guess the " << WORD_LENGTH;
 	cout << " letter isogram I'm thinking of?\n";
 	cout << endl;
+	return;
+}
 
-	// get a guess from the player
+// get a guess from the player
+// notice the side effects introduced. This function is playing two roles.
+string GetGuessAndPrintBack()
+{
 	cout << "Enter your guess: ";
 	string Guess = "";
 	getline(cin, Guess);
-
+	
 	// repeat the guess back to them
 	cout << "Your guess was: " << Guess << endl;
-
-	// enter a second guess
-	cout << "Enter your guess: ";
-	getline(cin, Guess);
-
-	// repeat the guess back to them
-	cout << "Your guess was: " << Guess << endl;
-
-	return 0;
+	return Guess;
 }
