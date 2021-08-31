9ebd84cd03ca09abec2c5cbe95fa9021c0408adf
mitchcamza <192mitch88@gmail.com>
2021-08-31 09:59:40 +0200

    Using do and while in C++

--- 02_BullCowGame/BullCowGame/main.cpp
+++ 02_BullCowGame/BullCowGame/main.cpp
@@ -13,9 +13,13 @@ bool AskToPlayAgain();
 
 int main()
 {
-	PrintIntro();	
-	PlayGame();
-	AskToPlayAgain();
+	bool bPlayAgain = false;
+	do
+	{
+		PrintIntro();
+		PlayGame();
+		bPlayAgain = AskToPlayAgain();
+	} while (bPlayAgain);	
 
 	return 0;
 }
@@ -55,7 +59,7 @@ string GetGuess()
 
 bool AskToPlayAgain()
 {
-	cout << "Do you want to play again? ";
+	cout << "Do you want to play again? (y/n): ";
 	string Response = "";
 	getline(cin, Response);
 	return (Response[0] == 'y') || (Response[0] == 'Y');
