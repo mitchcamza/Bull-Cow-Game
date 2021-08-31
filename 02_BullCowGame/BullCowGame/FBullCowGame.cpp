2ac4e4a71e7acdfb44ad5cfec48cdd1cfb22ddd6
mitchcamza <192mitch88@gmail.com>
2021-08-31 19:15:52 +0200

    Introducing the const Keyword

--- 02_BullCowGame/BullCowGame/FBullCowGame.cpp
+++ 02_BullCowGame/BullCowGame/FBullCowGame.cpp
@@ -1,9 +1,9 @@
 #include "FBullCowGame.h"
 
 
-int FBullCowGame::GetMaxTries() { return MyMaxTries; }
-int FBullCowGame::GetCurrentTry() { return MyCurrentTry; }
-bool FBullCowGame::IsGameWon() { return false; }
+int FBullCowGame::GetMaxTries() const { return MyMaxTries; }
+int FBullCowGame::GetCurrentTry() const { return MyCurrentTry; }
+bool FBullCowGame::IsGameWon() const { return false; }
 bool FBullCowGame::CheckGuessValidity(std::string) { return false; }
 
 void FBullCowGame::Reset()
--- 02_BullCowGame/BullCowGame/FBullCowGame.h
+++ 02_BullCowGame/BullCowGame/FBullCowGame.h
@@ -4,10 +4,11 @@
 class FBullCowGame
 {
 public:
+	int GetMaxTries() const;	
+	int GetCurrentTry() const;
+	bool IsGameWon() const;
+
 	void Reset();	// TODO: make a more rich return value
-	int GetMaxTries();	
-	int GetCurrentTry();
-	bool IsGameWon();
 	bool CheckGuessValidity(std::string); // TODO: make a more rich return value
 
 private:
